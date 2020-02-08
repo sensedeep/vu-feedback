1fb204aa19307f960dc346ceeadef4a4ee3b6ec4
Michael O'Brien <mob@emobrien.com>
2020-02-08 20:38:02 +1100

    DEV

--- VuFeedback.vue
+++ VuFeedback.vue
@@ -8,9 +8,7 @@
 import {Vue, Component, Prop} from 'vue-property-decorator'
 import {directive as onClick} from 'vue-clickaway'
 
-@Component({
-    directives: { onClick: onClick }
-})
+@Component({directives: {onClick: onClick}})
 export default class VuFeedback extends Vue {
 
     message = null
@@ -35,7 +33,6 @@ export default class VuFeedback extends Vue {
 
     error(msg) {
         this.init('error', msg)
-        this.severityStyle = `feedback-${this.severity}`
     }
 
     hasErrors() {
@@ -43,13 +40,15 @@ export default class VuFeedback extends Vue {
     }
 
     init(severity, msg) {
-        this.severity = severity
-        this.message = msg
+        setTimeout(() => {
+            this.severity = severity
+            this.severityStyle = `feedback-${severity}`
+            this.message = msg
+        }, 1)
     }
 
     info(msg) {
         this.init('info', msg)
-        this.severityStyle = `feedback-${this.severity}`
     }
 
     /*
@@ -62,9 +61,8 @@ export default class VuFeedback extends Vue {
     response(resp) {
         let feedback = resp.feedback || {}
         let message = resp.message || feedback.error || feedback.info
-        let severity = (resp.error || feedback.error) ? 'error' : 'info'
+        let severity = resp.severity || ((resp.error || feedback.error) ? 'error' : 'info')
         this.init(severity, message)
-        this.severityStyle = `feedback-${this.severity}`
     }
 
     warn(msg) {
@@ -74,67 +72,55 @@ export default class VuFeedback extends Vue {
 </script>
 
 <style lang="scss">
-.feedback-error {
-    background: #f55a4e;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    margin-left: -250px;
-    width: 500px;
-    height: 48px;
-    padding: 12px 3px 3px 3px;
-    color: rgba(255,255,255,0.85);
-    font-weight: bold;
-    font-size: 1.2em;
-    text-align: center;
-    z-index: 200;
-    background-color: #f55a4e;
-    background: -webkit-gradient(linear, left bottom, left top, color-stop(0, #f8877e), color-stop(1, #f22d1e));
-    background: -ms-linear-gradient(bottom, #f8877e, #f22d1e);
-    background: -moz-linear-gradient(center bottom, #f8877e 0%, #f22d1e 100%);
-    background: -o-linear-gradient(#f22d1e, #f8877e);
-    font-weight: bold;
-}
-.feedback-error:empty {
-    display: none;
-}
-.feedback-warn {
-    background: #fbc02d;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    margin-left: -250px;
-    width: 500px;
-    height: 48px;
-    padding: 12px 3px 3px 3px;
-    color: rgba(255,255,255,0.85);
-    font-weight: bold;
-    font-size: 1.2em;
-    text-align: center;
-    z-index: 200;
-}
-.feedback-warn:empty {
-    display: none;
-}
-.feedback-info {
-    background: #3f51b5;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    margin-left: -250px;
-    width: 500px;
-    height: 48px;
-    padding: 12px 3px 3px 3px;
-    color: rgba(255,255,255,0.85);
-    font-weight: bold;
-    font-size: 1.2em;
-    text-align: center;
-    z-index: 200;
-    top: -100px;
-    animation: feedback-transition ease-in 10s;
-}
-.feedback-info:empty {
-    display: none;
+.feedback {
+    span {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        margin-left: -200px;
+        width: 400px;
+        height: 48px;
+        padding: 12px 3px 3px 3px;
+        font-weight: bold;
+        font-size: 1.2em;
+        text-align: center;
+        z-index: 200;
+    }
+    .feedback-error {
+        background: #f55a4e;
+        color: rgba(255,255,255,0.85);
+        background-color: #f55a4e;
+        background: -webkit-gradient(linear, left bottom, left top, color-stop(0, #f8877e), color-stop(1, #f22d1e));
+        background: -ms-linear-gradient(bottom, #f8877e, #f22d1e);
+        background: -moz-linear-gradient(center bottom, #f8877e 0%, #f22d1e 100%);
+        background: -o-linear-gradient(#f22d1e, #f8877e);
+        z-index: 200;
+        top: -100px;
+        animation: feedback-transition ease-in 10s;
+    }
+    .feedback-error:empty {
+        display: none;
+    }
+    .feedback-warn {
+        background: #fbc02d;
+        color: rgba(255,255,255,0.85);
+        font-size: 1.2em;
+        top: -100px;
+        animation: feedback-transition ease-in 10s;
+    }
+    .feedback-warn:empty {
+        display: none;
+    }
+    //  THEME
+    .feedback-info {
+        background: #3f51b5;
+        color: rgba(255,255,255,0.85);
+        top: -100px;
+        animation: feedback-transition ease-in 10s;
+    }
+    .feedback-info:empty {
+        display: none;
+    }
 }
 @-moz-keyframes feedback-transition {
     0% {
